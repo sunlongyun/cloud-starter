cb9795e19a3b3da56a44f77e00a7497f1df2d30e
longyun.sun <longyun.sun@lianshang.com>
2018-10-28 17:43:01 +0800

    修改注解注释

--- lsCloudStarter-server/src/main/java/com/lianshang/cloud/server/annotation/EnableSeriesLogs.java
+++ lsCloudStarter-server/src/main/java/com/lianshang/cloud/server/annotation/EnableSeriesLogs.java
@@ -14,12 +14,9 @@ import com.lianshang.cloud.server.config.SeriesLogsConfigRegistrar;
 /**
  * 开启连续日志（可跨平台）功能
  * 
- * 1.同一一次请求，在平台的所有日志中统一加前缀“【前缀变量值】”
- * 2.跨平台访问，只要加参数传递过去，日志可以跨平台保持一致性
- * 3.参数传递形式，可以用header，cookie，或者请求地址追加参数
- * 4.value值是用户自定义的参数名称，默认lsReq
- * 5.日志配置的pattern格式中，加 %X{此处的参数值(value值)}，如果用户不自定义日志，系统默认配置的日志已经追加了日志标志符。
- * 6.从参数中可以获取日志前缀值，本地不会生成，否则随机生成一个日志前缀值
+ * 1.同一次请求，日志中统一加前缀“【前缀变量值】”
+ * 2.日志记录可以跨平台保持一致性
+ * 3.日志配置的pattern格式中，加 %X{lsReq}
  * @author 孙龙云
  */
 @Target({ElementType.TYPE})
@@ -28,9 +25,8 @@ import com.lianshang.cloud.server.config.SeriesLogsConfigRegistrar;
 @Import(SeriesLogsConfigRegistrar.class)
 public @interface EnableSeriesLogs {
 	/**
-	 * 连续注解的参数名称，可以是header中的字段，可以cookie，可以是参数。
-	 * 默认值 lsReq
-	 * 例如：url?lsReq=123456。那么系统中一次request请求中的日志都会自动添加【123456】
+	 * 参数名称(从参数名称获取“前缀变量值”)
+	 * 参数的传递可以通过 header、cookie或者url
 	 * @return
 	 */
 	public String value() default "lsReq";
