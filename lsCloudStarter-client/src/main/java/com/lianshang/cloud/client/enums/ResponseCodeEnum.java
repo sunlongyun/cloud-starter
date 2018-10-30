b5df181219927c6d3c04679e0c584354ae75d3f6
longyun.sun <longyun.sun@lianshang.com>
2018-10-30 20:11:15 +0800

    修改服务端处理插件

---
+++ lsCloudStarter-client/src/main/java/com/lianshang/cloud/client/enums/ResponseCodeEnum.java
@@ -0,0 +1,22 @@
+package flower.platform.constant;
+/**
+ * 返回码
+ * @author 孙龙云
+ *
+ */
+public enum ResponseCodeEnum {
+	SUCCESS("0000", "成功"), 
+	FAIL("9999", "失败");
+	private String code;
+	private String msg;
+	ResponseCodeEnum(String code, String msg){
+		this.code = code;
+		this.msg = msg;
+	}
+	public String code(){
+		return this.code;
+	}
+	public String msg(){
+		return this.msg;
+	}
+}
---
+++ lsCloudStarter-server/src/main/java/com/lianshang/cloud/server/beans/Response.java
@@ -0,0 +1,42 @@
+package com.lianshang.cloud.client.beans;
+
+import com.lianshang.cloud.client.enums.ResponseCodeEnum;
+import java.io.Serializable;
+import lombok.Data;
+
+/**
+ * 返回值
+ * @author 孙龙云
+ */
+@Data
+public class Response implements Serializable{
+	private static final long serialVersionUID = -7096601129585145021L;
+	private String code;
+	private String msg;
+	private Object data;
+	private Response(String code, String msg, Object data){
+		this.code = code;
+
+		this.msg = msg;
+		this.data = data;
+	}
+	public static Response success(){
+		return success("操作成功");
+	}
+	public static Response success(String msg){
+		Response res = new Response(ResponseCodeEnum.SUCCESS.code(), msg, null);
+		return res;
+	}
+	public static Response success(Object data){
+		Response res = new Response(ResponseCodeEnum.SUCCESS.code(), ResponseCodeEnum.SUCCESS.msg(), data);
+		return res;
+	}
+	public static Response fail(String msg){
+		Response res = new Response(ResponseCodeEnum.FAIL.code(), msg, null);
+		return res;
+	}
+	public static Response fail(){
+		return fail(ResponseCodeEnum.FAIL.msg());
+	}
+	
+}
